3c44113eb918090d9fdaa23428fef764208bdf54
Joe Bateman <joebob.bateman@gmail.com>
2026-01-27 10:52:10 -0600

    Run every stage from TextFileGen to Reco2 (MCC9.8) in one script

---
+++ scripts/run_larsoft.sh
@@ -0,0 +1,12 @@
+NEvents=$1
+outputDir="outputs/root/"
+OutName="tfg_hepmc_${NEvents}.root"
+
+lar -c fcls/TextFileGen.fcl -o ${outputDir}$OutName -n $NEvents
+lar -c wirecell_g4_uboone.fcl -s ${outputDir}$OutName -o ${outputDir}g4_$OutName -n $NEvents
+OutName=g4_$OutName
+lar -c wirecell_detsim_uboone.fcl -s ${outputDir}$OutName -o ${outputDir}detsim_$OutName -n $NEvents
+OutName=detsim_$OutName
+lar -c reco_uboone_mcc9_8_driver_stage1.fcl -s ${outputDir}$OutName -o ${outputDir}reco1_$OutName -n $NEvents
+OutName=reco1_$OutName
+lar -c reco_uboone_mcc9_8_driver_stage2_fullMC.fcl -s ${outputDir}$OutName -o ${outputDir}reco2_$OutName -n $NEvents
