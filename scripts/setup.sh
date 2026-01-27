ff4f956dedd018d21eb87107d9dfa453102bcb98
Joe Bateman <joebob.bateman@gmail.com>
2026-01-27 10:52:21 -0600

    Quick ups setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+setup_uboone
+setup root v6_28_12 -q e26:p3915:prof
+setup lhapdf v6_5_5 -q e26:p3915:prof
+setup pythia8 v8_3_13 -q e26:p3915:prof 
+setup log4cpp v1_1_3e -q e26:prof
+setup cmake v3_27_4
+setup uboonecode v10_04_07_20 -q e26:prof
