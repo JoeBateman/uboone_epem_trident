905e7718809628286f8bc69bffee3d5ac0a6c7a6
Joe Bateman <joebob.bateman@gmail.com>
2026-02-12 04:46:03 -0600

    Automating gen -> reco2

--- scripts/run_larsoft.sh
+++ scripts/run_larsoft.sh
@@ -1,5 +1,5 @@
 NEvents=$1
-outputDir="outputs/root/"
+outputDir="$DATA/DarkNews/Trident/outputs/root/"
 OutName="tfg_hepmc_${NEvents}.root"
 
 lar -c fcls/TextFileGen.fcl -o ${outputDir}$OutName -n $NEvents
@@ -10,3 +10,5 @@ OutName=detsim_$OutName
 lar -c reco_uboone_mcc9_8_driver_stage1.fcl -s ${outputDir}$OutName -o ${outputDir}reco1_$OutName -n $NEvents
 OutName=reco1_$OutName
 lar -c reco_uboone_mcc9_8_driver_stage2_fullMC.fcl -s ${outputDir}$OutName -o ${outputDir}reco2_$OutName -n $NEvents
+
+rm *.db *.log *.root *.pndr # Clean up intermediate files
